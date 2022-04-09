92bdbd1701ced8776edc96bea7d991f7f2b32f79
efevans <efevans1@gmail.com>
2022-04-08 23:35:06 -0700

    Add public method for controlling movement of characters

--- Assets/ShopKeeper/ShopKeeper.cs
+++ Assets/ShopKeeper/ShopKeeper.cs
@@ -6,20 +6,60 @@ public class ShopKeeper : MonoBehaviour
 {
     [SerializeField]
     private Animator Animator;
+    [SerializeField]
+    private float Speed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            var currState = Animator.GetBool("Trigger");
-            Animator.SetBool("Trigger", !currState);
-        } 
+            MoveToPoint(new Vector2(transform.position.x - 300, transform.position.y));
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            MoveToPoint(new Vector2(transform.position.x + 300, transform.position.y));
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            StartCoroutine(Cheer());
+        }
+    }
+
+    public IEnumerator Cheer()
+    {
+        Animator.SetTrigger("StartCheering");
+        yield return new WaitForSeconds(5);
+        Animator.SetTrigger("StopCheering");
+    }
+
+    public void MoveToPoint(Vector2 point)
+    {
+        StartCoroutine(MoveToPointEnumerator(point));
+    }
+
+    private IEnumerator MoveToPointEnumerator(Vector2 point)
+    {
+        Animator.SetTrigger("StartWalking");
+        SetFacedDirection(point);
+        while (true)
+        {
+            float speed = Speed * Time.deltaTime;
+            gameObject.transform.Translate(new Vector2(point.x - transform.position.x, point.y - transform.position.y).normalized * speed, Space.World);
+
+            if (Vector2.Distance(transform.position, point) < 1)
+            {
+                break;
+            }
+
+            yield return null;
+        }
+        Animator.SetTrigger("StopWalking");
+    }
+
+    private void SetFacedDirection(Vector2 point)
+    {
+        bool forward = transform.position.x < point.x;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0f, forward ? 0f : 180f, 0f));
     }
 }
